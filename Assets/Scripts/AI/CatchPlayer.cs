da272a3f98be413d9d16488d4af14ce649643777
killersshadow <32833605+killersshadow@users.noreply.github.com>
2019-01-11 12:06:07 +0100

    light detection system and start AI

---
+++ Assets/Scripts/AI/CatchPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CatchPlayer : MonoBehaviour
+{
+    public bool IsAtPlayer = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            IsAtPlayer = true;
+            Debug.Log("Exterminate");
+        }
+    }
+}
---
+++ Assets/Scripts/AI/MoveTowardsPos.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsPos : MonoBehaviour
+{
+    Vector3 Startpos;
+
+    public GameObject MyFlashlight;
+    public GameObject Player;
+
+    public float walkSpeed = 5;
+    public float runSpeed = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Startpos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfSpotted();
+        
+    }
+
+    private void CheckIfSpotted()
+    {
+        if (Startpos != transform.position && MyFlashlight.GetComponent<LightTargeting>().PlayerSighted)
+        {
+            float speed = walkSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Startpos, speed);
+        }
+        if (MyFlashlight.GetComponent<LightTargeting>().PlayerSighted && !GetComponent<CatchPlayer>().IsAtPlayer)
+        {
+            RunToPlayer();
+        }
+        
+    }
+
+    private void RunToPlayer()
+    {        
+        float speed = runSpeed * Time.deltaTime;            
+        transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed);        
+    }
+
+}
