cbb65e6be302af7217bd3210b334366a35799d0a
Brandon Ruigrok <31778356+No-hit-beastslaye@users.noreply.github.com>
2019-01-15 14:58:34 +0100

    Sneak V1.2

---
+++ Scripts/Movement/Crouch.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Brandon Ruigrok
+public class Crouch : MonoBehaviour
+{
+    //Audio clips for Crouching and standing up.
+    AudioSource audioData;
+    public AudioClip CrouchSound;
+    public AudioClip StandingSound;
+
+    public bool C = false;                 //The boolean for the crouch toggle.
+    public CapsuleCollider PlayerCollider;  //The hitbox for the player.
+    
+    public float StandingHeight = 1.95f;    //Standing hitbox
+    public float CrouchingHeight = 1.05f;   //Crouching hitbox
+
+    movement mov;
+
+    private void Start()
+    {
+        audioData = GetComponent<AudioSource>();    //Gets the audio source component on the gameobject.
+        mov = GetComponent<movement>();             //Gets the movement script.
+    }
+
+    void Update()
+    {
+        //From Standing to Crouching with the sound effect.
+        if (C == false && Input.GetKeyDown("c") && !Input.GetKey(KeyCode.LeftShift)) 
+        {
+            C = true;
+            mov.speed = mov.speed / 2;              //Changes walking speed to crouching speed.
+            audioData.clip = CrouchSound;
+            audioData.Play(0);
+        }
+
+        //From Crouching to Standing with the sound effect.
+        else if (C == true && Input.GetKeyDown("c"))
+        {
+            C = false;
+            mov.speed = mov.speed * 2;              //Changes crouching speed to walking speed.
+            audioData.clip = StandingSound;
+            audioData.Play(0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (C == true)
+            {
+                mov.speed = mov.speed * 2;
+                C = false;
+            }
+        }
+
+        //The hitbox for Standing to Crouching.
+        if (C == true && PlayerCollider.height >= CrouchingHeight)
+        {
+            PlayerCollider.height = PlayerCollider.height - 0.05f;
+        }
+
+        //The hitbox for Crouching to Standing.
+        else if (C == false && PlayerCollider.height <= StandingHeight)
+        {
+            PlayerCollider.height = PlayerCollider.height + 0.05f;
+        }
+    }
+}
---
+++ Scripts/Movement/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float speed;
+	public float rotateSpeed;
+
+	void Update () 
+	{
+		float xInput = Input.GetAxisRaw ("Horizontal");
+		float zInput = Input.GetAxisRaw ("Vertical");
+
+		Vector3 moveDirection = new Vector3 (0, 0, zInput);
+		Vector3 rotateDirection = new Vector3 (0, xInput, 0);
+		transform.Translate (moveDirection * speed * Time.deltaTime);
+		transform.Rotate (rotateDirection * rotateSpeed * Time.deltaTime);
+	}
+}
