69cafc9ca56050151937a5866857f2c0bfb4511b
sedzjad <stsheefe@gmail.com>
2019-02-02 20:38:05 +0100

    Fixed the guard not catching the player, removed OnCollisionEnter from winLose, map fixes.

--- Assets/Scripts/AI/CatchPlayer.cs
+++ Assets/Scripts/AI/CatchPlayer.cs
@@ -4,14 +4,21 @@ public class CatchPlayer : MonoBehaviour
 {
     [HideInInspector]
     public bool IsAtPlayer = false;
+    public GameObject Player;
+
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            Player.GetComponentsInChildren<CapsuleCollider>();
             IsAtPlayer = true;
             Vector3 pos = new Vector3(GetComponent<MoveTowardsPos>().Startpos.x, transform.position.y, GetComponent<MoveTowardsPos>().Startpos.z);
             transform.LookAt(pos);
+            if(IsAtPlayer == true)
+            {
+                Player.GetComponent<WinOrLose>().GameOver = true;
+            }
             
         }
     }
--- Assets/Scripts/WinLose/WinOrLose.cs
+++ Assets/Scripts/WinLose/WinOrLose.cs
@@ -6,31 +6,27 @@ using UnityEngine.SceneManagement;
 //Brandon Ruigrok
 public class WinOrLose : MonoBehaviour
 {
+
     //Strings with the scene names.
     private string Dead = "GameOver";
     private string Win = "EndGame";
 
     //Booleans for the win / lose conditions.
-    protected bool GameOver = false;
+    public bool GameOver = false;
     protected bool Winner = false;
 
     ZoneStatusUpdater Zone;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Gets the ZoneStatuses script
         Zone = GetComponent<ZoneStatusUpdater>();
+       
     }
 
-    //Lose condition
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "Enemy")
-        {
-            GameOver = true;
-        }
-    }
+
 
     // Update is called once per frame
     void Update()
